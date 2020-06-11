b9f18b735907e76c9e750adfe47c2c7c3abb3148
KiqECS <kaiquehelper2019@gmail.com>
2020-06-11 15:46:02 -0300

    Escape

---
+++ Aula6/aula06.py
@@ -0,0 +1,20 @@
+frase="O rato roeu a roupa do rei de roma"
+frasecomesoacos="  O rato roeu a roupa do rei de roma    "
+#print(frase.upper())
+#print(frase.lower())
+#print(frasecomesoacos.strip())
+#print(frase.split())
+"""
+fraselist=frase.split()
+print(fraselist[1],
+    fraselist[3],
+    fraselist[6]
+    )
+"""
+#print(frase.replace("rato","cachorro"))
+print(frase.__len__())
+print(frase[30:34])
+print(frasecomesoacos.__len__())
+print(frase[30:40])
+
+
---
+++ Randomcifra/randomnumber.py
@@ -0,0 +1,4 @@
+import random
+senha=1254559884515661
+cifra= random.randrange(senha)
+print(cifra)

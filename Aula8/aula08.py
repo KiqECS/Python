337761777dcfe74a6b6dda471e1a86ed87c4208b
KiqECS <kaiquehelper2019@gmail.com>
2020-06-13 13:01:14 -0300

    Bool

---
+++ Aula8/aula08.py
@@ -0,0 +1,11 @@
+logico= 10>20
+numerofalso= 0
+numeroverdade= 1 # >1
+vazio= "" #False
+preenchido= "a" #True
+
+print(bool(logico))
+print(bool(numerofalso))
+print(bool(numeroverdade))
+print(bool(vazio))
+print(bool(preenchido))

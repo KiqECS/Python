e32dcc8ba5d273458241b9db61085c8ac38bcf9b
KiqECS <kaiquehelper2019@gmail.com>
2020-06-15 15:37:02 -0300

    If

---
+++ Aula10/aula10.py
@@ -0,0 +1,10 @@
+#nome="Kaique"
+nome="Cleitu"
+dia=15
+ano=2020
+print("Você é o \"CRIADOR\"? ")
+if nome=="Kaique" and dia==15 and ano==2020:
+    print("Sim, você é")
+else:
+    print("Saia daqui seu plágio")
+print("Obrigado ou não por fazer o teste!!")

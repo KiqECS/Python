6d1b953adb3527f7a1d5ad2f17c70fc361fcd1c9
KiqECS <kaiquehelper2019@gmail.com>
2020-06-18 11:31:36 -0300

    Elif (and, or)

---
+++ Aula11/aula11.py
@@ -0,0 +1,9 @@
+palcool="cachaça"
+aperitivo="carne de porco"
+print("Bar do Tigrão\n")
+if (palcool == "cachaça" and aperitivo=="carne de boi"):
+    print("Bar de qualidade")
+elif (aperitivo == "carne de porco" and (palcool == "cachaça" or palcool=="Gim")):
+    print("Qualidade normal")
+else:
+    print("Bem ruim")

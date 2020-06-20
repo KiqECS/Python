2d783279ab184256432ab6e40d0f4e558563375d
KiqECS <kaiquehelper2019@gmail.com>
2020-06-20 09:53:37 -0300

    For

---
+++ Aula12/aula12.py
@@ -0,0 +1,8 @@
+nome = ["Claudio", "Cleitu", "Kaique", "Santana"]
+
+for lista in nome:
+    print(lista)
+    if (lista == "Kaique"):
+        print("We find us creator")
+        break
+print("Fim do Programa")

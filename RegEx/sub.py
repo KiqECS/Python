c1fee191086b04694fb3685b9b90d0e7d8bb8328
Kiqcoffee <kaiquehelper2019@gmail.com>
2020-10-21 11:53:10 -0300

    Mudando coisas no texto

--- RegEx/sub.py
+++ RegEx/sub.py
@@ -3,4 +3,5 @@ import re
 texto = "Lorem ipsum mi phasellus dictumst egestas nunc primis auctor est, ac vivamus sagittis tincidunt velit vivamus iaculis nam at ac, integer enim habitant urna conubia auctor risus inceptos. ut porta lobortis gravida porta aliquam primis pharetra, egestas malesuada ornare blandit curabitur erat nullam integer, libero eget viverra ornare phasellus bibendum. "
 
 chan = re.sub("\s","   .",texto)
+chan = re.sub("a","y",texto)
 print(chan)

d2c56a772439a240436fc58064375c33350d65f9
Kiqcoffee <kaiquehelper2019@gmail.com>
2020-10-21 11:52:28 -0300

    Mudando itens no texto

---
+++ RegEx/sub.py
@@ -0,0 +1,6 @@
+import re
+
+texto = "Lorem ipsum mi phasellus dictumst egestas nunc primis auctor est, ac vivamus sagittis tincidunt velit vivamus iaculis nam at ac, integer enim habitant urna conubia auctor risus inceptos. ut porta lobortis gravida porta aliquam primis pharetra, egestas malesuada ornare blandit curabitur erat nullam integer, libero eget viverra ornare phasellus bibendum. "
+
+chan = re.sub("\s","   .",texto)
+print(chan)

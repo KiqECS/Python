2496ce0ba8cd3da6d5fe669ce376ed373f2c3930
Kiqcoffee <kaiquehelper2019@gmail.com>
2020-10-22 09:59:44 -0300

    Feio, mas interessante

---
+++ Archives/testgen.py
@@ -0,0 +1,10 @@
+f_name_html= "test.html"
+f_html = open("/home/kiq/LearnWhatever/Python/Archives/" + f_name_html,"wt")
+f_html.write("<!DOCTYPE html>\n<html>\n\t<head>\n\t<meta charset=\"UTF-8\">\t\n</head>\n\t<body>\n\t\t<p>Olá, eu venho do PY</p>\n\t</body>\n</html>")
+
+"""
+f_name_css = "test.css"
+f_css = open("/home/kiq/LearnWhatever/Python/Archives/" + f_name_css,"xt")
+f_css.write("body{\ntext-align:\"center\"\n}")
+"""
+# De certa forma é um gerador de estrutura html kkkkk
